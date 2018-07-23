b46588653d9c32e96b383e5cd22cd41e3f5479bc
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-22 20:20:11 -0400

    1

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,9 +4,12 @@
 $(document).ready(function() {
 
 var searchTerms = []
+var searchEmpty = true
+
 function displayGiphs() {
     $("#img-field").empty()
     var giph = $("#search-item").val().trim();
+    if (giph != ""){
     searchTerms.push(giph)
     $("#btn-field").append("<button type='button' class='btn btn-info'id="+giph+">"+giph+"</button>")
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giph + "&rating=g&fmt=json&api_key=NCWFcjgphTcq8gGzLPS918neSnqMDRDO&limit=10";
@@ -16,14 +19,11 @@ function displayGiphs() {
       url: queryURL,
       method: "GET"
     }).then(function(result) {
-        var newP = $("<p>")
-        var rating = result.rating
-
         result.data.forEach(element => {
             $("#img-field").append("<img src="+element.images.fixed_height.url+">")
         });
     })
-    
+}
 }; // end function
 
 $("#btn-giph-search").click(displayGiphs)
@@ -34,9 +34,7 @@ window.addEventListener("keypress", enterSearch, false);
 function enterSearch(e) {
     if (e.keyCode == 13 ) {
     displayGiphs()
-} else {
-    console.log(e.keyCode)
-}
+} 
 }
 
 
