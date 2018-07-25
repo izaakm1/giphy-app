95dcd698662c38c0cc70d39b18a83b6a0084d090
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-24 22:40:13 -0400

    something respectible

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,38 +4,79 @@
 $(document).ready(function() {
 
 var searchTerms = []
-var searchEmpty = true
+var btnPress = false
+var searchType = "";
+var findGiph = "";
 
-function displayGiphs() {
+window.addEventListener("keypress", enterKeyIsHit, false)
+
+// if enter is hit
+function enterKeyIsHit(e) {
+    if ($("#search-item").val().trim()!="") {
+        if (e.keyCode == 13 ) {
+        findGiph = $("#search-item").val().trim();
+        displayGiphs()
+        }   
+    }    
+}
+// if search button is clicked
+$(document).on("click", "#btn-giph-search",function(){
+    searchType = "search-button";
+    findGiph = $("#search-item").val().trim();
+    console.log(findGiph)
+    displayGiphs()
+});
+// if field button is clicked
+$(document).on("click", ".giphResult-btn",function(){
+    searchType = "field-button"
+    findGiph = $(this).attr("id")
+    console.log($(this).attr("id"))
+    displayGiphs()
+});
+
+function displayGiphs(type) {
+    // empty image field
     $("#img-field").empty()
-    var giph = $("#search-item").val().trim();
-    if (giph != ""){
-    searchTerms.push(giph)
-    $("#btn-field").append("<button type='button' class='btn btn-info'id="+giph+">"+giph+"</button>")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giph + "&rating=g&fmt=json&api_key=NCWFcjgphTcq8gGzLPS918neSnqMDRDO&limit=10";
 
-    // Creating an AJAX call for giph retreivale
+    // determine what triggered function, search btn click or field btn click
+    switch (searchType) {
+
+        case "search-button" :
+        console.log("search button")
+        break;
+
+        case "field-button" :
+        console.log("field-button")
+        break;
+
+        default : 
+        break;
+    }
+    // if not already a part of the button field array, add it to it
+    if (searchTerms.indexOf(findGiph) == -1){
+        searchTerms.push(findGiph)
+        $("#btn-field").append("<button type='button' class='btn btn-info giphResult-btn'id="+findGiph+">"+findGiph+"</button>")
+        }    
+
+    // *** AJAX Section ***
+    // Creating an AJAX call for giph retrieval
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + findGiph + "&rating=g&fmt=json&api_key=NCWFcjgphTcq8gGzLPS918neSnqMDRDO&limit=10";
+   
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(result) {
+        console.log(result)
         result.data.forEach(element => {
-            $("#img-field").append("<img src="+element.images.fixed_height.url+">")
+            // $("#img-field").append("<span><img src="+element.images.fixed_height.url+"></span>")
+            $("#img-field").append(`<span class='card' id=`+findGiph+`style='width: 14rem;'>\<img class='card-img-top' src=`+element.images.fixed_height.url+`>\<span class='card-body'>\<span class='card-text'>`+element.title+` Rating: `+element.rating+` </span></span></span>`)
         });
     })
-}
 }; // end function
 
-$("#btn-giph-search").click(displayGiphs)
 
 
-window.addEventListener("keypress", enterSearch, false);
-
-function enterSearch(e) {
-    if (e.keyCode == 13 ) {
-    displayGiphs()
-} 
-}
 
 
-});
+}); //document ready closing tag
