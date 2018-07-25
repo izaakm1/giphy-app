d5055afc1b0b7015344799df7d8a60f358e24eea
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-24 23:08:23 -0400

    again

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -21,10 +21,12 @@ function enterKeyIsHit(e) {
 }
 // if search button is clicked
 $(document).on("click", "#btn-giph-search",function(){
-    searchType = "search-button";
-    findGiph = $("#search-item").val().trim();
-    console.log(findGiph)
-    displayGiphs()
+    if ($("#search-item").val().trim()!="") {
+        searchType = "search-button";
+        findGiph = $("#search-item").val().trim();
+        console.log(findGiph)
+        displayGiphs()
+    }
 });
 // if field button is clicked
 $(document).on("click", ".giphResult-btn",function(){
@@ -33,25 +35,15 @@ $(document).on("click", ".giphResult-btn",function(){
     console.log($(this).attr("id"))
     displayGiphs()
 });
+//if random button is selected
+$(document).on("click", "#btn-giph-random-search",function(){
+    displayRandomGiphs()
+});
 
-function displayGiphs(type) {
+function displayGiphs() {
     // empty image field
     $("#img-field").empty()
 
-    // determine what triggered function, search btn click or field btn click
-    switch (searchType) {
-
-        case "search-button" :
-        console.log("search button")
-        break;
-
-        case "field-button" :
-        console.log("field-button")
-        break;
-
-        default : 
-        break;
-    }
     // if not already a part of the button field array, add it to it
     if (searchTerms.indexOf(findGiph) == -1){
         searchTerms.push(findGiph)
@@ -70,12 +62,30 @@ function displayGiphs(type) {
         console.log(result)
         result.data.forEach(element => {
             // $("#img-field").append("<span><img src="+element.images.fixed_height.url+"></span>")
-            $("#img-field").append(`<span class='card' id=`+findGiph+`style='width: 14rem;'>\<img class='card-img-top' src=`+element.images.fixed_height.url+`>\<span class='card-body'>\<span class='card-text'>`+element.title+` Rating: `+element.rating+` </span></span></span>`)
+            $("#img-field").append(`<span class='card' id=`+findGiph+`style='width: 14rem;'>\<img class='card-img-top' src=`+element.images.fixed_width.url+`>\<span class='card-body'>\<span class='card-text'>`+element.title+` Rating: `+element.rating+` </span></span></span>`)
         });
     })
 }; // end function
 
+function displayRandomGiphs() {
+    $("#img-field").empty()
+    // *** AJAX Section ***
+    // Creating an AJAX call for giph retrieval
 
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=NCWFcjgphTcq8gGzLPS918neSnqMDRDO&rating=g&fmt=json&";
+   
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(result) {
+        console.log(result)
+
+        // result.data.forEach(element => {
+            // $("#img-field").append("<span><img src="+element.images.fixed_height.url+"></span>")
+            $("#img-field").append(`<span class='card' id=`+result.data.title+`style='width: 14rem;'>\<img class='card-img-top' src=`+result.data.images.fixed_height.url+`>\<span class='card-body'>\<span class='card-text'>`+result.data.title+` Rating: `+result.data.rating+` </span></span></span>`)
+        // });
+    })
+}//end random function
 
 
 
